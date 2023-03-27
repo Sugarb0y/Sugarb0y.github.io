9dd272c7cbaa054acd4359aad6d82cf1f027c0d3
Sugarb0y <61544214+Sugarb0y@users.noreply.github.com>
2023-03-27 17:46:26 +0200

    Add files via upload

--- test/script.js
+++ test/script.js
@@ -2,6 +2,17 @@
 
 $(document).ready(function(){
   
+  import { VRButton } from 'three/addons/webxr/VRButton.js';
+
+	import { XRControllerModelFactory } from 'three/addons/webxr/XRControllerModelFactory.js';
+
+	let container;
+	let camera, scene, renderer;
+	let controller1, controller2;
+	let controllerGrip1, controllerGrip2;
+	
+	let raycaster;
+	
   var scene, camera, renderer;
   var width = 500;
   var height = 500;
@@ -14,12 +25,78 @@ $(document).ready(function(){
     // Aspect ratio - width of the element divided by the height
     // Near and far clipping ends
     
-    camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 10000 );
+    scene.background = new THREE.Color( 0x808080 );
+
+    camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 10 );
+    camera.position.set( 0, 1.6, 3 );
+
+    const floorGeometry = new THREE.PlaneGeometry( 4, 4 );
+    const floorMaterial = new THREE.MeshStandardMaterial( {
+            color: 0xeeeeee,
+            roughness: 1.0,
+            metalness: 0.0
+    } );
+    const floor = new THREE.Mesh( floorGeometry, floorMaterial );
+    floor.rotation.x = - Math.PI / 2;
+    floor.receiveShadow = true;
+    scene.add( floor );
+
+    scene.add( new THREE.HemisphereLight( 0x808080, 0x606060 ) );
+
+    const light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 0, 6, 0 );
+    light.castShadow = true;
+    light.shadow.camera.top = 2;
+    light.shadow.camera.bottom = - 2;
+    light.shadow.camera.right = 2;
+    light.shadow.camera.left = - 2;
+    light.shadow.mapSize.set( 4096, 4096 );
+    scene.add( light );
     
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( width, height ); //set the size of the model - more is more tasking for the browser
-    document.body.appendChild( renderer.domElement ); //create the canvas
-  
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.shadowMap.enabled = true;
+    renderer.xr.enabled = true;
+    container.appendChild( renderer.domElement );
+
+    document.body.appendChild( VRButton.createButton( renderer ) );
+	
+	ontroller1 = renderer.xr.getController( 0 );
+    controller1.addEventListener( 'selectstart', onSelectStart );
+    controller1.addEventListener( 'selectend', onSelectEnd );
+    scene.add( controller1 );
+
+    controller2 = renderer.xr.getController( 1 );
+    controller2.addEventListener( 'selectstart', onSelectStart );
+    controller2.addEventListener( 'selectend', onSelectEnd );
+    scene.add( controller2 );
+
+    const controllerModelFactory = new XRControllerModelFactory();
+
+    controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+    controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+    scene.add( controllerGrip1 );
+
+    controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+    controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+    scene.add( controllerGrip2 );
+
+    //
+
+    const geometry = new THREE.BufferGeometry().setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 1 ) ] );
+
+    const line = new THREE.Line( geometry );
+    line.name = 'line';
+    line.scale.z = 5;
+
+    controller1.add( line.clone() );
+    controller2.add( line.clone() );
+
+    raycaster = new THREE.Raycaster();
+
+
     // Circle
     /*var circleGeometry = new THREE.CircleGeometry( 10, 32, 0, Math.PI/2 );
     var circle = new THREE.Mesh( circleGeometry, material );
